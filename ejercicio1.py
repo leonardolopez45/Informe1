0deece68eb7c3c7fd61068aa97f86dc29472016c
Your Name <you@example.com>
2021-11-22 17:20:28 -0500

    informe1

---
+++ ejercicio1.py
@@ -0,0 +1,4 @@
+#valor ecuacion
+x=float(input("ingrese el valor de x para la ecuacion y=3x^3-2x^2+3x+1   "))
+
+print("el valor de y es", 3*x**3-2*x**2+3*x-1)
---
+++ ejercicio3.py
@@ -0,0 +1,10 @@
+#km a millas 
+print("ingrese el valor de 0 si quiere pasar de millas a km, y el valor de 1 si quiere pasar de km a millas")
+x=(input("ingrese la opcion de conversion  "))
+
+if(x=="0"):
+  z=float(input("ingrese la valor a convertir  "))
+  print(w ,  "en valor de kilometros es ", w*1.61)
+elif(x=="1"):
+  w=float(input("ingrese la valor a convertir  "))
+  print(w ,"en valor de millas es  ", w/1.61)
